f9ebd52f3e3e8c95cb98ca52a1ef2dc752e3dba9
Douglas G <douglas@gadeco.com.br>
2016-03-07 08:02:52 -0300

    Fix missing files during build, copying them now

--- blobs/Android.mk
+++ blobs/Android.mk
@@ -10,67 +10,103 @@ SYMLINKS := A500CG_BLOBS_SYMLINKS
 
 # PowerVR SGX544 + Redhookbay
 $(SYMLINKS):
+	@echo "Create Directory Structure: $(TARGET_OUT)/vendor/"
+	$(hide) mkdir -p "$(TARGET_OUT)/vendor/"
+	@echo "Create Directory Structure: $(TARGET_OUT)/vendor/lib/"
+	$(hide) mkdir -p "$(TARGET_OUT)/vendor/lib/"
+	@echo "Create Directory Structure: $(TARGET_OUT)/vendor/lib/egl/"
+	$(hide) mkdir -p "$(TARGET_OUT)/vendor/lib/egl/"
+	@echo "Create Directory Structure: $(TARGET_OUT)/vendor/lib/hw/"
+	$(hide) mkdir -p "$(TARGET_OUT)/vendor/lib/hw/"
+	@echo "Copy: vendor/asus/a500cg/proprietary/system/vendor/lib/egl/libEGL_POWERVR_SGX544_115.so.1.12.3197934 -> $(TARGET_OUT)/vendor/lib/egl/libEGL_POWERVR_SGX544_115.so.1.12.3197934"
+	$(hide) cp -f "$(LOCAL_PATH)/../../vendor/asus/a500cg/proprietary/system/vendor/lib/egl/libEGL_POWERVR_SGX544_115.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/egl/libEGL_POWERVR_SGX544_115.so.1.12.3197934"
+	@echo "Copy: vendor/asus/a500cg/proprietary/system/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX544_115.so.1.12.3197934 -> $(TARGET_OUT)/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX544_115.so.1.12.3197934"
+	$(hide) cp -f "$(LOCAL_PATH)/../../vendor/asus/a500cg/proprietary/system/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX544_115.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX544_115.so.1.12.3197934"
+	@echo "Copy: vendor/asus/a500cg/proprietary/system/vendor/lib/egl/libGLESv2_POWERVR_SGX544_115.so.1.12.3197934 -> $(TARGET_OUT)/vendor/lib/egl/libGLESv2_POWERVR_SGX544_115.so.1.12.3197934"
+	$(hide) cp -f "$(LOCAL_PATH)/../../vendor/asus/a500cg/proprietary/system/vendor/lib/egl/libGLESv2_POWERVR_SGX544_115.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/egl/libGLESv2_POWERVR_SGX544_115.so.1.12.3197934"
+	@echo "Copy: vendor/asus/a500cg/proprietary/system/vendor/lib/libglslcompiler.so.1.12.3197934 -> $(TARGET_OUT)/vendor/lib/libglslcompiler.so.1.12.3197934"
+	$(hide) cp -f "$(LOCAL_PATH)/../../vendor/asus/a500cg/proprietary/system/vendor/lib/libglslcompiler.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/libglslcompiler.so.1.12.3197934"
+	@echo "Copy: vendor/asus/a500cg/proprietary/system/vendor/lib/libIMGegl.so.1.12.3197934 -> $(TARGET_OUT)/vendor/lib/libIMGegl.so.1.12.3197934"
+	$(hide) cp -f "$(LOCAL_PATH)/../../vendor/asus/a500cg/proprietary/system/vendor/lib/libIMGegl.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/libIMGegl.so.1.12.3197934"
+	@echo "Copy: vendor/asus/a500cg/proprietary/system/vendor/lib/liboclcompiler.so.1.12.3197934 -> $(TARGET_OUT)/vendor/lib/liboclcompiler.so.1.12.3197934"
+	$(hide) cp -f "$(LOCAL_PATH)/../../vendor/asus/a500cg/proprietary/system/vendor/lib/liboclcompiler.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/liboclcompiler.so.1.12.3197934"
+	@echo "Copy: vendor/asus/a500cg/proprietary/system/vendor/lib/libpvr2d.so.1.12.3197934 -> $(TARGET_OUT)/vendor/lib/libpvr2d.so.1.12.3197934"
+	$(hide) cp -f "$(LOCAL_PATH)/../../vendor/asus/a500cg/proprietary/system/vendor/lib/libpvr2d.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/libpvr2d.so.1.12.3197934"
+	@echo "Copy: vendor/asus/a500cg/proprietary/system/vendor/lib/libpvrANDROID_WSEGL.so.1.12.3197934 -> $(TARGET_OUT)/vendor/lib/libpvrANDROID_WSEGL.so.1.12.3197934"
+	$(hide) cp -f "$(LOCAL_PATH)/../../vendor/asus/a500cg/proprietary/system/vendor/lib/libpvrANDROID_WSEGL.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/libpvrANDROID_WSEGL.so.1.12.3197934"
+	@echo "Copy: vendor/asus/a500cg/proprietary/system/vendor/lib/libPVROCL.so.1.12.3197934 -> $(TARGET_OUT)/vendor/lib/libPVROCL.so.1.12.3197934"
+	$(hide) cp -f "$(LOCAL_PATH)/../../vendor/asus/a500cg/proprietary/system/vendor/lib/libPVROCL.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/libPVROCL.so.1.12.3197934"
+	@echo "Copy: vendor/asus/a500cg/proprietary/system/vendor/lib/libPVRScopeServices.so.1.12.3197934 -> $(TARGET_OUT)/vendor/lib/libPVRScopeServices.so.1.12.3197934"
+	$(hide) cp -f "$(LOCAL_PATH)/../../vendor/asus/a500cg/proprietary/system/vendor/lib/libPVRScopeServices.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/libPVRScopeServices.so.1.12.3197934"
+	@echo "Copy: vendor/asus/a500cg/proprietary/system/vendor/lib/libsrv_init.so.1.12.3197934 -> $(TARGET_OUT)/vendor/lib/libsrv_init.so.1.12.3197934"
+	$(hide) cp -f "$(LOCAL_PATH)/../../vendor/asus/a500cg/proprietary/system/vendor/lib/libsrv_init.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/libsrv_init.so.1.12.3197934"
+	@echo "Copy: vendor/asus/a500cg/proprietary/system/vendor/lib/libsrv_um.so.1.12.3197934 -> $(TARGET_OUT)/vendor/lib/libsrv_um.so.1.12.3197934"
+	$(hide) cp -f "$(LOCAL_PATH)/../../vendor/asus/a500cg/proprietary/system/vendor/lib/libsrv_um.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/libsrv_um.so.1.12.3197934"
+	@echo "Copy: vendor/asus/a500cg/proprietary/system/vendor/lib/libusc.so.1.12.3197934 -> $(TARGET_OUT)/vendor/lib/libusc.so.1.12.3197934"
+	$(hide) cp -f "$(LOCAL_PATH)/../../vendor/asus/a500cg/proprietary/system/vendor/lib/libusc.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/libusc.so.1.12.3197934"
+	@echo "Copy: vendor/asus/a500cg/proprietary/system/vendor/lib/hw/gralloc.redhookbay.so.1.12.3197934 -> $(TARGET_OUT)/vendor/lib/hw/gralloc.redhookbay.so.1.12.3197934"
+	$(hide) cp -f "$(LOCAL_PATH)/../../vendor/asus/a500cg/proprietary/system/vendor/lib/hw/gralloc.redhookbay.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/hw/gralloc.redhookbay.so.1.12.3197934"
+	@echo "Copy: vendor/asus/a500cg/proprietary/system/vendor/lib/hw/memtrack.redhookbay.so.1.12.3197934 -> $(TARGET_OUT)/vendor/lib/hw/memtrack.redhookbay.so.1.12.3197934"
+	$(hide) cp -f "$(LOCAL_PATH)/../../vendor/asus/a500cg/proprietary/system/vendor/lib/hw/memtrack.redhookbay.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/hw/memtrack.redhookbay.so.1.12.3197934"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/egl/libEGL_POWERVR_SGX544_115.so -> libEGL_POWERVR_SGX544_115.so.1.12.3197934"
-	$(hide) ln -sf libEGL_POWERVR_SGX544_115.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/egl/libEGL_POWERVR_SGX544_115.so
+	$(hide) ln -sf "libEGL_POWERVR_SGX544_115.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/egl/libEGL_POWERVR_SGX544_115.so"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX544_115.so -> libGLESv1_CM_POWERVR_SGX544_115.so.1.12.3197934"
-	$(hide) ln -sf libGLESv1_CM_POWERVR_SGX544_115.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX544_115.so
+	$(hide) ln -sf "libGLESv1_CM_POWERVR_SGX544_115.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX544_115.so"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/egl/libGLESv2_POWERVR_SGX544_115.so -> libGLESv2_POWERVR_SGX544_115.so.1.12.3197934"
-	$(hide) ln -sf libGLESv2_POWERVR_SGX544_115.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/egl/libGLESv2_POWERVR_SGX544_115.so
+	$(hide) ln -sf "libGLESv2_POWERVR_SGX544_115.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/egl/libGLESv2_POWERVR_SGX544_115.so"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/libglslcompiler.so -> libglslcompiler.so.1.12.3197934"
-	$(hide) ln -sf libglslcompiler.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/libglslcompiler.so
+	$(hide) ln -sf "libglslcompiler.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/libglslcompiler.so"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/lib/IMGegl.so -> libIMGegl.so.1.12.3197934"
-	$(hide) ln -sf libIMGegl.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/libIMGegl.so
+	$(hide) ln -sf "libIMGegl.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/libIMGegl.so"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/liboclcompiler.so -> liboclcompiler.so.1.12.3197934"
-	$(hide) ln -sf liboclcompiler.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/liboclcompiler.so
+	$(hide) ln -sf "liboclcompiler.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/liboclcompiler.so"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/libpvr2d.so -> libpvr2d.so.1.12.3197934"
-	$(hide) ln -sf libpvr2d.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/libpvr2d.so
+	$(hide) ln -sf "libpvr2d.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/libpvr2d.so"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/libpvrANDROID_WSEGL.so -> libpvrANDROID_WSEGL.so.1.12.3197934"
-	$(hide) ln -sf libpvrANDROID_WSEGL.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/libpvrANDROID_WSEGL.so
+	$(hide) ln -sf "libpvrANDROID_WSEGL.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/libpvrANDROID_WSEGL.so"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/libPVROCL.so -> libPVROCL.so.1.12.3197934"
-	$(hide) ln -sf libPVROCL.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/libPVROCL.so
+	$(hide) ln -sf "libPVROCL.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/libPVROCL.so"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/libPVRScopeServices.so -> libPVRScopeServices.so.1.12.3197934"
-	$(hide) ln -sf libPVRScopeServices.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/libPVRScopeServices.so
+	$(hide) ln -sf "libPVRScopeServices.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/libPVRScopeServices.so"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/libsrv_init.so -> libsrv_init.so.1.12.3197934"
-	$(hide) ln -sf libsrv_init.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/libsrv_init.so
+	$(hide) ln -sf "libsrv_init.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/libsrv_init.so"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/libsrv_um.so -> libsrv_um.so.1.12.3197934"
-	$(hide) ln -sf libsrv_um.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/libsrv_um.so
+	$(hide) ln -sf "libsrv_um.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/libsrv_um.so"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/libusc.so -> libusc.so.1.12.3197934"
-	$(hide) ln -sf libusc.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/libusc.so
+	$(hide) ln -sf "libusc.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/libusc.so"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/hw/gralloc.redhookbay.so -> gralloc.redhookbay.so.1.12.3197934"
-	$(hide) ln -sf gralloc.redhookbay.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/hw/gralloc.redhookbay.so
+	$(hide) ln -sf "gralloc.redhookbay.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/hw/gralloc.redhookbay.so"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/hw/memtrack.redhookbay.so -> memtrack.redhookbay.so.1.12.3197934"
-	$(hide) ln -sf memtrack.redhookbay.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/hw/memtrack.redhookbay.so
-
+	$(hide) ln -sf "memtrack.redhookbay.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/hw/memtrack.redhookbay.so"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/egl/libEGL_POWERVR_SGX544_115.so.1 -> libEGL_POWERVR_SGX544_115.so.1.12.3197934"
-	$(hide) ln -sf libEGL_POWERVR_SGX544_115.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/egl/libEGL_POWERVR_SGX544_115.so.1
+	$(hide) ln -sf "libEGL_POWERVR_SGX544_115.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/egl/libEGL_POWERVR_SGX544_115.so.1"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX544_115.so.1 -> libGLESv1_CM_POWERVR_SGX544_115.so.1.12.3197934"
-	$(hide) ln -sf libGLESv1_CM_POWERVR_SGX544_115.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX544_115.so.1
+	$(hide) ln -sf "libGLESv1_CM_POWERVR_SGX544_115.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX544_115.so.1"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/egl/libGLESv2_POWERVR_SGX544_115.so.1 -> libGLESv2_POWERVR_SGX544_115.so.1.12.3197934"
-	$(hide) ln -sf libGLESv2_POWERVR_SGX544_115.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/egl/libGLESv2_POWERVR_SGX544_115.so.1
+	$(hide) ln -sf "libGLESv2_POWERVR_SGX544_115.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/egl/libGLESv2_POWERVR_SGX544_115.so.1"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/libglslcompiler.so.1 -> libglslcompiler.so.1.12.3197934"
-	$(hide) ln -sf libglslcompiler.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/libglslcompiler.so.1
+	$(hide) ln -sf "libglslcompiler.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/libglslcompiler.so.1"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/lib/IMGegl.so.1 -> libIMGegl.so.1.12.3197934"
-	$(hide) ln -sf libIMGegl.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/libIMGegl.so.1
+	$(hide) ln -sf "libIMGegl.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/libIMGegl.so.1"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/liboclcompiler.so.1 -> liboclcompiler.so.1.12.3197934"
-	$(hide) ln -sf liboclcompiler.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/liboclcompiler.so.1
+	$(hide) ln -sf "liboclcompiler.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/liboclcompiler.so.1"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/libpvr2d.so.1 -> libpvr2d.so.1.12.3197934"
-	$(hide) ln -sf libpvr2d.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/libpvr2d.so.1
+	$(hide) ln -sf "libpvr2d.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/libpvr2d.so.1"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/libpvrANDROID_WSEGL.so.1 -> libpvrANDROID_WSEGL.so.1.12.3197934"
-	$(hide) ln -sf libpvrANDROID_WSEGL.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/libpvrANDROID_WSEGL.so.1
+	$(hide) ln -sf "libpvrANDROID_WSEGL.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/libpvrANDROID_WSEGL.so.1"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/libPVROCL.so.1 -> libPVROCL.so.1.12.3197934"
-	$(hide) ln -sf libPVROCL.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/libPVROCL.so.1
+	$(hide) ln -sf "libPVROCL.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/libPVROCL.so.1"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/libPVRScopeServices.so.1 -> libPVRScopeServices.so.1.12.3197934"
-	$(hide) ln -sf libPVRScopeServices.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/libPVRScopeServices.so.1
+	$(hide) ln -sf "libPVRScopeServices.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/libPVRScopeServices.so.1"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/libsrv_init.so.1 -> libsrv_init.so.1.12.3197934"
-	$(hide) ln -sf libsrv_init.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/libsrv_init.so.1
+	$(hide) ln -sf "libsrv_init.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/libsrv_init.so.1"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/libsrv_um.so.1 -> libsrv_um.so.1.12.3197934"
-	$(hide) ln -sf libsrv_um.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/libsrv_um.so.1
+	$(hide) ln -sf "libsrv_um.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/libsrv_um.so.1"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/libusc.so.1 -> libusc.so.1.12.3197934"
-	$(hide) ln -sf libusc.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/libusc.so.1
+	$(hide) ln -sf "libusc.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/libusc.so.1"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/hw/gralloc.redhookbay.so.1 -> gralloc.redhookbay.so.1.12.3197934"
-	$(hide) ln -sf gralloc.redhookbay.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/hw/gralloc.redhookbay.so.1
+	$(hide) ln -sf "gralloc.redhookbay.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/hw/gralloc.redhookbay.so.1"
 	@echo "Symlink: $(TARGET_OUT)/vendor/lib/hw/memtrack.redhookbay.so.1 -> memtrack.redhookbay.so.1.12.3197934"
-	$(hide) ln -sf memtrack.redhookbay.so.1.12.3197934 $(TARGET_OUT)/vendor/lib/hw/memtrack.redhookbay.so.1
-
+	$(hide) ln -sf "memtrack.redhookbay.so.1.12.3197934" "$(TARGET_OUT)/vendor/lib/hw/memtrack.redhookbay.so.1"
 
 ALL_DEFAULT_INSTALLED_MODULES += $(SYMLINKS)
